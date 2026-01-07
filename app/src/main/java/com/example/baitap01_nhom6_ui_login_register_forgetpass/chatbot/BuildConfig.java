27f95d52f001492a2abcdafa2f31491fd63bf754
vietk5 <148680923+vietk5@users.noreply.github.com>
2026-01-07 21:31:05 +0700

    api key

--- app/src/main/java/com/example/baitap01_nhom6_ui_login_register_forgetpass/chatbot/BuildConfig.java
+++ app/src/main/java/com/example/baitap01_nhom6_ui_login_register_forgetpass/chatbot/BuildConfig.java
@@ -1,5 +1,5 @@
 package com.example.baitap01_nhom6_ui_login_register_forgetpass.chatbot;
 
 public class BuildConfig {
-    public static String apiKey = "AIzaSyAAsQNa9-GV8t2l9Wr07_rTKTHGFwuBR-g";
+    public static String apiKey = "AIzaSyAXmQyn2mC9p4u_IQ2XQaR6NAE6XtOez9I";
 }
